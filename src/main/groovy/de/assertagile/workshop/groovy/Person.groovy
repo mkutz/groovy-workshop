1bb95c9bda9ad0971eefec45548599ded8d3d575
Michael Kutz <michael.kutz@rewe-digital.com>
2019-04-08 12:54:00 +0200

    Solved 1.3

--- src/main/groovy/de/assertagile/workshop/groovy/Person.groovy
+++ src/main/groovy/de/assertagile/workshop/groovy/Person.groovy
@@ -2,63 +2,45 @@ package de.assertagile.workshop.groovy
 
 import java.time.LocalDate
 
-public class Person {
+class Person {
 
-    private final String name;
-    private final LocalDate birthday;
-    private String emailAddress;
+    final String name
+    final LocalDate birthday
+    String emailAddress
 
-    public Person(String name, LocalDate birthday, String emailAddress) {
-        this(name, birthday);
-        this.emailAddress = emailAddress;
+    Person(String name, LocalDate birthday, String emailAddress = null) {
+        this.name = name
+        this.birthday = birthday
+        this.emailAddress = emailAddress
     }
 
-    public Person(String name, LocalDate birthday) {
-        this.name = name;
-        this.birthday = birthday;
+    boolean isOfAge() {
+        !LocalDate.now().minusYears(18).isBefore(birthday)
     }
 
-    public String getName() {
-        return name;
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (getClass() != o.class) return false
+
+        Person person = (Person) o
+
+        if (birthday != person.birthday) return false
+        if (emailAddress != person.emailAddress) return false
+        if (name != person.name) return false
+
+        return true
     }
 
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public boolean isOfAge() {
-        return !LocalDate.now().minusYears(18).isBefore(birthday);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this.is(o)) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person that = (Person) o;
-        return name.equals(that.name) &&
-                birthday.equals(that.birthday) &&
-                Objects.equals(emailAddress, that.emailAddress);
+    int hashCode() {
+        int result
+        result = name.hashCode()
+        result = 31 * result + birthday.hashCode()
+        result = 31 * result + (emailAddress != null ? emailAddress.hashCode() : 0)
+        result
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, birthday, emailAddress);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", birthday=" + birthday +
-                ", emailAddress='" + emailAddress + '\'' +
-                '}';
+    String toString() {
+        "Person{name='$name', birthday=$birthday, emailAddress='$emailAddress'}"
     }
 }
