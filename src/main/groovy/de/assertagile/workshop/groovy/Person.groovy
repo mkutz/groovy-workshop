5c54ef6aa61316dedb7a6210092f20d32ecd60d8
Michael Kutz <michael.kutz@rewe-digital.com>
2019-04-10 09:45:36 +0200

    Solved 4.1

--- src/main/groovy/de/assertagile/workshop/groovy/Person.groovy
+++ src/main/groovy/de/assertagile/workshop/groovy/Person.groovy
@@ -1,7 +1,12 @@
 package de.assertagile.workshop.groovy
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
 import java.time.LocalDate
 
+@EqualsAndHashCode
+@ToString
 class Person {
 
     final String name
@@ -16,31 +21,5 @@ class Person {
 
     boolean isOfAge() {
         !LocalDate.now().minusYears(18).isBefore(birthday)
-    }
-
-    boolean equals(o) {
-        if (this.is(o)) return true
-        if (getClass() != o.class) return false
-
-        Person person = (Person) o
-
-        if (birthday != person.birthday) return false
-        if (emailAddress != person.emailAddress) return false
-        if (name != person.name) return false
-
-        return true
-    }
-
-    int hashCode() {
-        int result
-        result = name.hashCode()
-        result = 31 * result + birthday.hashCode()
-        result = 31 * result + (emailAddress != null ? emailAddress.hashCode() : 0)
-        result
-    }
-
-    @Override
-    String toString() {
-        "Person{name='$name', birthday=$birthday, emailAddress='$emailAddress'}"
     }
 }
